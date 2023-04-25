2a92461630a02704719ee31cf02e504a69d819b0
qiyana-ratchet <wereter31@konkuk.ac.kr>
2023-04-26 01:41:45 +0900

    Design: Modify theme and letter spacing

--- frontend/news-vision/src/components/Footer/styles.ts
+++ frontend/news-vision/src/components/Footer/styles.ts
@@ -1,28 +1,45 @@
 import styled from 'styled-components';
 
 export const FooterWrapper = styled.footer`
-display: flex;
-flex-direction: column;
-background-color: #f5f5f5;
-color: #7a7a7a;
-padding: 20px;
-font-size: 14px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #4B4B4B;
+  color: white;
+  padding: 36px;
+  font-size: 14px;
+  height: 152px;
 `;
 
 export const FooterContent = styled.div`
-display: flex;
-align-items: center;
-justify-content: space-between;
-margin-bottom: 20px;
+  font-family: 'Prociono', serif;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-bottom: 20px;
+  max-width: 1240px;
 `;
 
 export const FooterLinks = styled.div`
-display: flex;
-flex-wrap: wrap;
+  font-family: arial, serif, 'Prociono';
+  display: flex;
+  flex-wrap: wrap;
+  margin-bottom: 17px;
+  letter-spacing: 0.1px;
 
-a {
-  color: #7a7a7a;
-  text-decoration: none;
-  margin-right: 20px;
-}
+
+  a {
+    color: white;
+    text-decoration: none;
+    margin-right: 30px;
+  }
+
+`;
+
+export const Copyright = styled.div`
+  font-family: arial, serif, 'Prociono';
+  font-size: 13px;
+  line-height: 13px;
+  text-align: left;
+  letter-spacing: 0.2px;
 `;
