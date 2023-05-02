35474861057c1433f9fad796d4dae98fb60fc7e2
qiyana-ratchet <wereter31@konkuk.ac.kr>
2023-05-02 14:47:46 +0900

    Feat: Add CORS configuration for frontend development environment

--- src/main/java/com/example/newsvision/support/Policy.java
+++ src/main/java/com/example/newsvision/support/Policy.java
@@ -3,8 +3,8 @@ package com.example.newsvision.support;
 
 public class Policy {
 
-    public static final int articleCountsByOneApi = 3;
-    public static final int pageSize = 5;
+    public static final int articleCountsByOneApi = 24;
+    public static final int pageSize = 8;
     public static String apiUrl = "https://ars.yna.co.kr/api/v2/nstat?total=Y&lang=KR&body=Y&count=%s&sdate=%s";
     public static final String imgSource = "https://img.yna.co.kr";
 
